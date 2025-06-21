2f85adecaed87def6a6034762d8decb52cc200ae
Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>
2025-06-21 21:38:10 +0100

    Initial commits
    Signed-off-by: Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>

---
+++ src/test/java/com/aiho/springTransactions/SpringTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiho.springTransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
